2b9fd6b1b48d1782315cd92ed0f4bafcce7da7e9
gtook101 <78670851+gtook101@users.noreply.github.com>
2021-05-15 23:03:07 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -6,22 +6,22 @@ const ProductTag = require("./ProductTag");
 const { beforeBulkDestroy } = require("./Product");
 
 // Products belongsTo Category
-Product.belongsTo(Category, { foreignKey: "category_id" });
+Product.belongsTo(Category, { foreignKey: "category_id", onDelete: "CASCADE" });
 
 // Categories have many Products
-Category.hasMany(Product, { foreignKey: "products_id" });
+Category.hasMany(Product, { foreignKey: "category_id" });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: "product_id",
-  as: "product_tags",
+  //as: "product_tags",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: "tag_products",
+  // as: "product_tags",
   foreignKey: "tag_id",
 });
 
